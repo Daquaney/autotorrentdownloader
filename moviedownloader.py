228dc4c6dd49466fb681a3aa274709169861759f
Tristan Edwards <38468893+Daquaney@users.noreply.github.com>
2019-03-14 23:44:31 -0700

    Add files via upload

--- moviedownloader.py
+++ moviedownloader.py
@@ -1,40 +1,38 @@
 # made by tristan edwards
 import feedparser
 import transmissionrpc
-# rss feed of top 100 movie torrents
 url = 'https://thepiratebay.org/rss/top100/207'
-# transmission client setup input ip!!!
-tc = transmissionrpc.Client('IP HERE!!!', port=9091)
+tc = transmissionrpc.Client('192.168.1.91', port=9091)
 
-# to check if link is in the database of magnets
-def link_in_db(link):
+
+def link_in_db(link_db):
     with open('magnets.txt', 'r') as database:
         for line in database:
-            if link in line:
+            if link_db in line:
                 return True
     return False
 
 
 feed = feedparser.parse(url)
-links_to_print = []
+links_to_grab = []
 links_to_skip = []
-# if magnet already exists in database do nothing, otherwise add the link to the database
+
 for link in feed.entries:
     link = link.link
     if link_in_db(link):
         links_to_skip.append(link)
     else:
-        links_to_print.append(link)
-# write to the database
+        links_to_grab.append(link)
+
 link_file = open('magnets.txt', 'a')
-for link in links_to_print:
+for link in links_to_grab:
     if not link_in_db(link):
         if '720p' in link:
             continue
         link_file.write(link + "\n")
 link_file.close()
-# if the link is 720p skip, otherwise start downloading the torrent
-for link in links_to_print:
+
+for link in links_to_grab:
     if '720p' in link:
         continue
     print(link)
